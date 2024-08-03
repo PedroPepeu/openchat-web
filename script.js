7a718ae305c65e3d8ca98601da30810450fc408d
PedroPepeu <pedrosteamgtav@gmail.com>
2024-08-03 12:39:44 -0400

    start

---
+++ script.js
@@ -0,0 +1,15 @@
+const button_send_info = getElementById("button_login");
+
+function check_get_info(email, password) {
+    
+}
+
+button_send_info.addEventListener("click", function () {
+    const email_get_info = getElementById("email_login");
+    const password_get_info = getElementById("password_login");
+
+    if(!check_get_info(email_get_info, password_get_info)) {
+
+    }
+});
+
